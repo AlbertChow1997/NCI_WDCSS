017d2daf22c84ef2c37ca5d249aa99980d73f102
ManassesM <mouramana6@gmail.com>
2025-11-09 17:52:18 +0000

    contact page: map positioning fixed

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,11 +1,10 @@
-class Contact{
-   
-    static createContactMap(){
-        return`
+class Contact {
+    static createContactMap() {
+        return `
             <div>
-                <iframe class="map-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2382.13521547134!2d-6.2429928!3d53.3489292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e8cd86e97e7%3A0x6d51b774ee7fa935!2sNational%20College%20of%20Ireland!5e0!3m2!1sen!2sie!4v1690000000000">
+                <iframe class=" w-100 rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2382.13521547134!2d-6.2429928!3d53.3489292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e8cd86e97e7%3A0x6d51b774ee7fa935!2sNational%20College%20of%20Ireland!5e0!3m2!1sen!2sie!4v1690000000000">
                 </iframe>
             </div>
-      `;  
+      `;
     }
 }
